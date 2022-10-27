b2f9bd3e563b585bb05cb633b91a4d248ae62c6c
Miriam Gordillo-Chavez <miriam.gordillo-chavez@united.com>
2022-10-26 22:05:38 -0500

    practice useReducer hook

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,21 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import Card from '../UI/Card/Card';
 import styles from './Counter.module.css';
 
-function Counter(props) {
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement'
+    
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment' :
+            return { count: state.count + 1}
+        case 'decrement' :
+            return { count: state.count - 1}
+            default:
+                return state
+    }
+     
+}
+
+function Counter() {
+    const [state, dispatch] = useReducer(reducer, {count: 0})
     const [count, setCount] = useState(0);
 
     const decrementCount = () => {
-        setCount(prevCount => prevCount -1);
+        dispatch( { type: ACTIONS.DECREMENT });
     }
     const incrementCount = () => {
-        setCount(prevCount => prevCount +1);
+        dispatch( { type: ACTIONS.INCREMENT });
     }
     return (
         <Card>
         <div className={styles.counter}>
         <button className={styles.button} onClick={decrementCount}> - </button>
-        <span> {count} </span>
+        <span> {state.count} </span>
         <button className={styles.button} onClick={incrementCount}> + </button>
         </div>
         </Card>
